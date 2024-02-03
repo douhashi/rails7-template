c8096b1708faa7799f07661422c66ef8dc78cc27
Sho DOUHASHI <douhashi@gmail.com>
2024-02-04 06:36:53 +0900

    refactor sidebar preview

--- app/frontend/components/ui/sidebar/preview.rb
+++ app/frontend/components/ui/sidebar/preview.rb
@@ -5,5 +5,16 @@ class Ui::Sidebar::Preview < ApplicationViewComponentPreview
   # self.container_class = "w-1/2 border border-gray-300"
 
   def default
+    render_with(menu_items: menu_items)
+  end
+
+  private
+
+  def menu_items
+    [
+      { name: 'Nav Item 1', url: '#', icon: 'fa-house', active: true  },
+      { name: 'Nav Item 2', url: '#', icon: 'fa-house', active: false },
+      { name: 'Nav Item 3', url: '#', icon: 'fa-house', active: false }
+    ]
   end
 end
