33aaa22f3e7519b5d2f9c8daed3a79fa4e051fb1
Sho DOUHASHI <douhashi@gmail.com>
2024-02-12 03:50:20 +0900

    header component

--- app/frontend/components/ui/global_action_panel/profile_dropdown/component.rb
+++ app/frontend/components/ui/global_action_panel/profile_dropdown/component.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Ui::GlobalActionPanel::ProfileDropdown::Component < ApplicationViewComponent
+  option :username
+  option :avatar_url
+  
+  renders_one :dropdown_content
 end
