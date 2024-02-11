d60f7eb0ef7f13be89bdd7b032f47a99fd76a986
Sho DOUHASHI <douhashi@gmail.com>
2024-02-11 23:21:04 +0900

    refactor nav_item

--- app/frontend/components/ui/sidebar/component.rb
+++ app/frontend/components/ui/sidebar/component.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Ui::Sidebar::Component < ApplicationViewComponent
-  renders_one :brand
-  renders_many :nav_items
+  renders_one :brand, Ui::Sidebar::Brand::Component
+  renders_many :nav_items, Ui::Sidebar::NavItem::Component
 end
