a2317f279ad05b4b043a496fac8dc1e260adbb02
Sho DOUHASHI <douhashi@gmail.com>
2024-02-12 18:39:16 +0900

    remove unused components

--- app/frontend/components/ui/heading/component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Ui::Heading::Component < ApplicationViewComponent
-  option :title
-end
--- app/frontend/components/ui/heading_with_description/component.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Ui::HeadingWithDescription::Component < Ui::Heading::Component
-  option :description
-end
