39edbb4352098962c8b532d9523a59fb9a31bfce
Sho DOUHASHI <douhashi@gmail.com>
2024-02-14 04:08:46 +0900

    add badge styles

--- app/frontend/components/ui/badge/preview.rb
+++ app/frontend/components/ui/badge/preview.rb
@@ -4,6 +4,10 @@ class Ui::Badge::Preview < ApplicationViewComponentPreview
   # You can specify the container class for the default template
   # self.container_class = "w-1/2 border border-gray-300"
 
-  def default
+  # @param text text "Badge text"
+  # @param color select { choices: [primary, secondary, teritary, destructive] }
+  # @param size select { choices: [xs, sm, base, lg, xl] }
+  def default(text: 'Text', color: 'primary', size: 'xs')
+    render_with(text:, color:, size:)
   end
 end
