c9ab95b1bbfeeee4a07b030524f93f0c69867b0c
Sho DOUHASHI <douhashi@gmail.com>
2024-02-14 03:26:25 +0900

    button styles

--- app/frontend/components/ui/button/component.rb
+++ app/frontend/components/ui/button/component.rb
@@ -2,4 +2,54 @@
 
 class Ui::Button::Component < ApplicationViewComponent
   option :label, default: -> { 'Button' }
+  option :style, default: -> { 'primary' }
+  option :size,  default: -> { 'base' }
+
+  def css_classes
+    "#{base_css} #{color_css} #{size_css}"
+  end
+
+  private
+
+  def base_css
+    "border-2 transition ease-in duration-200 text-center shadow-md rounded-md"
+  end
+
+  def color_css
+    case style
+    when 'primary'
+      text_color   = "text-white"
+      bg_color     = "bg-indigo-600 hover:bg-indigo-500"
+      border_color = "border-indigo-600 hover:border-indigo-500"
+    when 'secondary'
+      text_color   = "text-indigo-600 hover:text-white dark:text-indigo-500 dark:hover:text-white"
+      bg_color     = "hover:bg-indigo-500 dark:hover:bg-indigo-500"
+      border_color = "border-indigo-600 dark:border-indigo-500"
+    when 'teritary'
+      text_color   = "text-gray-600 hover:text-white dark:text-gray-500 dark:hover:text-white"
+      bg_color     = "hover:bg-gray-500 dark:hover:bg-gray-500"
+      border_color = "border-gray-600 dark:border-gray-500"
+    when 'destructive'
+      text_color   = "text-red-600 hover:text-white dark:text-red-500 dark:hover:text-white"
+      bg_color     = "hover:bg-red-500 dark:hover:bg-red-500"
+      border_color = "border-red-600 dark:border-red-500"
+    end
+
+    "#{text_color} #{bg_color} #{border_color}"
+  end
+
+  def size_css
+    case size
+    when 'xs'
+      "py-1 px-1 text-xs font-normal"
+    when 'sm'
+      "py-1 px-2 text-sm font-normal"
+    when 'base'
+      "py-1 px-2 text-base font-normal"
+    when 'lg'
+      "py-1 px-2 text-lg font-normal"
+    when 'xl'
+      "py-2 px-4 text-xl font-semibold"
+    end
+  end
 end
--- app/frontend/components/ui/button/preview.rb
+++ app/frontend/components/ui/button/preview.rb
@@ -4,6 +4,54 @@ class Ui::Button::Preview < ApplicationViewComponentPreview
   # You can specify the container class for the default template
   # self.container_class = "w-1/2 border border-gray-300"
 
-  def default
+  # @param label text "Label text"
+  # @param style select { choices: [primary, secondary, teritary, destructive] }
+  # @param size select { choices: [xs, sm, base, lg, xl] }
+  def default(label: 'This is a button', style: 'primary', size: 'base')
+    render_with(style:, label:, size:)
   end
+
+  # @!group Styles
+  # @param label text "Label text"
+  # @param size select { choices: [xs, sm, base, lg, xl] }
+  def primary(label: 'This is a button', size: 'base')
+    render_with(style: 'primary', label:, size:)
+  end
+
+  def secondary(label: 'This is a button', size: 'base')
+    render_with(style: 'secondary', label:, size:)
+  end
+
+  def teritary(label: 'This is a button', size: 'base')
+    render_with(style: 'teritary', label:, size:)
+  end
+
+  def destructive(label: 'This is a button', size: 'base')
+    render_with(style: 'destructive', label:, size:)
+  end
+  # @!endgroup Styles
+
+  # @!group Sizes
+  # @param label text "Label text"
+  # @param style select { choices: [primary, secondary, teritary, destructive] }
+  def xs(label: 'Button', style: 'primary')
+    render_with(label:, style:, size: 'xs')
+  end
+
+  def sm(label: 'Button', style: 'primary')
+    render_with(label:, style:, size: 'sm')
+  end
+
+  def base(label: 'Button', style: 'primary')
+    render_with(label:, style:, size: 'base')
+  end
+
+  def lg(label: 'Button', style: 'primary')
+    render_with(label:, style:, size: 'lg')
+  end
+
+  def xl(label: 'Button', style: 'primary')
+    render_with(label:, style:, size: 'xl')
+  end
+  # @!endgroup Styles
 end
