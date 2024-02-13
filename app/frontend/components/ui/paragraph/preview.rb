cea19e5730a23e3aa443845917c22daf4a4c83f2
Sho DOUHASHI <douhashi@gmail.com>
2024-02-14 01:59:35 +0900

    default option

--- app/frontend/components/ui/paragraph/preview.rb
+++ app/frontend/components/ui/paragraph/preview.rb
@@ -4,6 +4,7 @@ class Ui::Paragraph::Preview < ApplicationViewComponentPreview
   # You can specify the container class for the default template
   # self.container_class = "w-1/2 border border-gray-300"
 
+  # @param content text "Content text"
   def default(content: "This is a paragraph")
     render_with(content: content)
   end
