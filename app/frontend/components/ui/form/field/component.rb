165d996efde9462bb5d1a446a5ffd149677dc16b
Sho DOUHASHI <douhashi@gmail.com>
2024-02-18 21:15:17 +0900

    fix broken specs

--- app/frontend/components/ui/form/field/component.rb
+++ app/frontend/components/ui/form/field/component.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Ui::Form::Field::Component < Ui::Form::BaseComponent
-  option :field_type, default: -> { "text" }
+  option :field_type, default: -> { "text_field" }
 end
--- app/frontend/components/ui/form/field_with_icon/preview.rb
+++ app/frontend/components/ui/form/field_with_icon/preview.rb
@@ -4,12 +4,12 @@ class Ui::Form::FieldWithIcon::Preview < ApplicationViewComponentPreview
   # You can specify the container class for the default template
   # self.container_class = "w-1/2 border border-gray-300"
 
-  # @param field_type select { choices: [text, email, password, date, datetime] }
+  # @param field_type select { choices: [text_field, email_field, password_field, date_field, datetime_field] }
   # @param icon text "Icon class"
   # @param required toggle "Required flag"
   # @param disabled toggle "Disabled flag"
   # @param placeholder text "Placeholder text"
-  def default(field_type: "text", icon: "fas fa-user", placeholder: "placeholder text", required: false, disabled: false)
+  def default(field_type: "text_field", icon: "fas fa-user", placeholder: "placeholder text", required: false, disabled: false)
     html_options = { placeholder: placeholder, required: required, disabled: disabled }
     render_with(icon: icon, field_type: field_type, html_options: html_options)
   end
